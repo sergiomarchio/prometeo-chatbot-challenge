775755a96b8f6f503c2e64d62ab8228eccac373c
Sergio Marchio <sergiomarchio@gmail.com>
2022-09-08 00:41:56 -0300

    added login page

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 
 
 def homepage(request):
-    return HttpResponse(_("Hello!"))
+    return render(request, 'chatbot/login.html')
