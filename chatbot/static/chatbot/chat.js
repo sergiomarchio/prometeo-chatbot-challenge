b674eb6d48ac03654c55339e375cb7d626404856
Sergio Marchio <sergiomarchio@gmail.com>
2022-09-15 21:07:14 -0300

    improved js and css file organization

--- chatbot/static/chatbot/chat.js
+++ chatbot/static/chatbot/chat.js
@@ -1,19 +1,3 @@
-// getCookie() taken from https://docs.djangoproject.com/en/4.1/howto/csrf/
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 
 function addNewMessage(message) {
     let newMessage = document.createElement("div");
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = 'chatbot'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('guest/', views.guest, name='guest'),
-    path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('chat/', views.chat, name='chat'),
     path('chat/process_message/', views.process_message, name='process_message'),
